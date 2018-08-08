1426003b5465b27faaa2db43fc84e9099c724283
root <root@localhost.localdomain>
2018-08-08 22:08:37 +0800

    add jingdong computer spider

---
+++ computer/computer-spider.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+baseUrl = "https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv4160&productId=6949959&score=0&sortType=5&page=0&pageSize=10"
+
+func main() {
+	fmt.Println("hello world")
+}
